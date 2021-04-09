214b0e810f4a63ec33aca857a0335d1a8974a82a
windiaOne <82230057+windiaOne@users.noreply.github.com>
2021-04-09 18:56:53 +0200

    Create fightSimulation.js
    initial goal

---
+++ fightSimulation.js
@@ -0,0 +1,2 @@
+// I want a small fight simulation based on the HH++ script: it should calculate me all "used setups" on girls and equipment I iterate through before performing a league fight.
+// instead of makin thus many screenshots, I want to do 1(!) screenshot at best. And then go and set up alpha and equipment.
